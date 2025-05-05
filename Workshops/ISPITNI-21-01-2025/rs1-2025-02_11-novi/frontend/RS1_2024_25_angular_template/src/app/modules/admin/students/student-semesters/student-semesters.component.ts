1a0bb6bb7790b179d9df78f662416a0ab6a8f392
Vedad Keskin <vedad.keskin98@gmail.com>
2025-05-05 16:01:06 +0200

    2A.3

--- Workshops/ISPITNI-21-01-2025/rs1-2025-02_11-novi/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Workshops/ISPITNI-21-01-2025/rs1-2025-02_11-novi/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,5 +1,10 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {
+  StudentGetByIdEndpointService, StudentGetByIdResponse
+} from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
+import {MySnackbarHelperService} from '../../../shared/snackbars/my-snackbar-helper.service';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-student-semesters',
@@ -11,20 +16,39 @@ import {ActivatedRoute} from '@angular/router';
 export class StudentSemestersComponent implements OnInit{
 
   studentId:any;
+  //student:any;
+  student: StudentGetByIdResponse | null = null;
 
 
   constructor(
     private route: ActivatedRoute,
-    ) {}
+    private studentGetByIdEndpointService:StudentGetByIdEndpointService,
+    private snackbar: MySnackbarHelperService,
+    private router: Router,
+    private dialog: MatDialog,
+    ) {
+
+    this.studentId = this.route.snapshot.params['id'];
+    //this.studentId = this.route.snapshot.paramMap.get('id');
+  }
 
   ngOnInit(): void {
 
-      this.studentId = this.route.snapshot.params['id'];
-      //this.studentId = this.route.snapshot.paramMap.get('id');
+    this.fetchStudent();
 
     }
 
 
+  private fetchStudent() {
 
+    this.studentGetByIdEndpointService.handleAsync(this.studentId).subscribe({
+      next: (student) => {
 
+        this.student = student;
+
+      },
+      error: (error) => console.error('Error fetching student data', error),
+    });
+
+  }
 }
