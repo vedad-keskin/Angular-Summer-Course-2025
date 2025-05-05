858927da0ff84b8174dc646d4a3ddb90ec58b9fc
Vedad Keskin <vedad.keskin98@gmail.com>
2025-05-05 15:48:38 +0200

    2A.2

--- Workshops/ISPITNI-21-01-2025/rs1-2025-02_11-novi/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Workshops/ISPITNI-21-01-2025/rs1-2025-02_11-novi/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-student-semesters',
   standalone: false,
-  
+
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
-export class StudentSemestersComponent {
+export class StudentSemestersComponent implements OnInit{
+
+  studentId:any;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    ) {}
+
+  ngOnInit(): void {
+
+      this.studentId = this.route.snapshot.params['id'];
+      //this.studentId = this.route.snapshot.paramMap.get('id');
+
+    }
+
+
+
 
 }
